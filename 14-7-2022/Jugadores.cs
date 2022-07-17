cf0d09e691aead56d8c28c46a04c0ec426fe2203
Facundo Grasso <facudok@gmail.com>
2022-07-17 17:51:11 -0300

    Reorganice folders in proyect

---
+++ 14-7-2022/Jugadores.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDeClase
+{
+    public abstract class Jugador
+    {
+        public int Velocidad { get; set; }
+        public int Tiro { get; set; }
+        public int Regate { get; set; }
+        public int Defensa { get; set; }
+        public int Pase { get; set; }
+        public int Fisico { get; set; }
+
+        public Jugador(int velocidad, int tiro, int regate, int defensa, int pase, int fisico)
+        {
+            Velocidad = velocidad;
+            Tiro = tiro;
+            Regate = regate;
+            Defensa = defensa;
+            Pase = pase;
+            Fisico = fisico;
+        }
+    }
+    public interface IJugador
+    {
+        string DevolverVelocidad();
+    }
+    public class Ronaldo : Jugador, IJugador
+    {
+        public Ronaldo(int velocidad, int tiro, int regate, int defensa, int pase, int fisico)
+            : base(velocidad, tiro, regate, defensa, pase, fisico)
+        {
+        }
+
+        public string DevolverVelocidad()
+        {
+            return $"Velocidad de Ronaldo {Velocidad}";
+        }
+    }
+
+    public class Messi : Jugador, IJugador
+    {
+        public Messi(int velocidad, int tiro, int regate, int defensa, int pase, int fisico)
+            : base(velocidad, tiro, regate, defensa, pase, fisico)
+        {
+        }
+
+        public string DevolverVelocidad()
+        {
+            return $"Valocidad de Messi {Velocidad}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EjemploDeClase
+{
+    public class ProbarObjetos
+    {
+        static void Main(string[] args)
+        {
+            // ejemlpo de figuras geometricas del 13-7-2022
+            Cubo cubo = new Cubo(5);
+            Rectangulo rectangulo = new Rectangulo(3, 6);
+            Circulo circulo = new Circulo(6);
+
+
+            Console.WriteLine("volumen cubo:" + cubo.Volumen());
+
+
+            // ejemlpo de jugadores del 14-7-2022
+            Ronaldo ronaldo = new Ronaldo(90, 30, 95, 15, 90, 50);
+            Messi messi = new Messi(88, 95, 89, 20, 90, 70);
+            Console.WriteLine(messi.DevolverVelocidad());
+            Console.WriteLine(ronaldo.DevolverVelocidad());
+        }
+    }
+}
