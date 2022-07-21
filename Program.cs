fc12049831ff6f70e344c81298e975fbaaed8b7c
Facundo Grasso <facudok@gmail.com>
2022-07-21 10:29:05 -0300

    Clase del 20 - 7 - 2022

--- Program.cs
+++ Program.cs
@@ -4,33 +4,22 @@ namespace EjemploDeClase
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Dictionary<string, double>> menuPorSecciones
-                = new Dictionary<string, Dictionary<string, double>>();
+            ProductosConE productosConE = new ProductosConE();
 
-            menuPorSecciones.Add("Bebidas sin alcohol", new Dictionary<string, double>
+            ProductoConPropiedadesPublicas[] productos =
             {
-                {"Jugos   ", 90 },
-                {"Gaseosas", 85 },
-                {"Agua    ", 95 }
-            });
+                new ProductoConPropiedadesPublicas(1, "Yogurt descremado"),
+                new ProductoConPropiedadesPublicas(2, "Leche"),
+                new ProductoConPropiedadesPublicas(3, "Huevos"),
+                new ProductoConPropiedadesPublicas(4, "Vino"),
+                new ProductoConPropiedadesPublicas(5, "Sal"),
+                new ProductoConPropiedadesPublicas(6, "Estufa"),
+            };
 
-            menuPorSecciones.Add("Bebidas aclholicas", new Dictionary<string, double>
-            {
-                {"Daikiris", 91 },
-                {"Vinos   ", 86 },
-                {"Cerveza ", 96 }
-            });
-
-            Console.WriteLine("Seccion\t\t\t\t\tProducto\t\tPrecio");
-            foreach (KeyValuePair<string, Dictionary<string, double>> seccion in menuPorSecciones)
-            {
-                Console.WriteLine("{0}", seccion.Key);
-
-                foreach (KeyValuePair<string, double> productoYPrecio in seccion.Value)
-                {
-                    Console.WriteLine("\t\t\t\t\t{0}\t\t{1}", productoYPrecio.Key.ToUpper(), productoYPrecio.Value);
-                }
-            }
+            productosConE.InsertarProductosConE(productos);
+            productosConE.MostrarLista();
+            productosConE.LimpiarLista();
+            productosConE.MostrarLista();
         }
     }
 }
