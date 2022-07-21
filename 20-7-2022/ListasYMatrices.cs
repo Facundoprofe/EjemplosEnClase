91fb7035f292fd4448253ab308919906a1135f73
Facundo Grasso <facudok@gmail.com>
2022-07-21 10:33:18 -0300

    Agregado de ejemplo de productos con E

--- 20-7-2022/ListasYMatrices.cs
+++ 20-7-2022/ListasYMatrices.cs
@@ -47,6 +47,24 @@ namespace EjemploDeClase._20_7_2022
 
             ejemploMatriz.Ingresar();
             ejemploMatriz.Imprimir();
+
+            //Ejemplo de productos con e
+            ProductosConE productosConE = new ProductosConE();
+
+            ProductoConPropiedadesPublicas[] productos =
+            {
+                new ProductoConPropiedadesPublicas(1, "Yogurt descremado"),
+                new ProductoConPropiedadesPublicas(2, "Leche"),
+                new ProductoConPropiedadesPublicas(3, "Huevos"),
+                new ProductoConPropiedadesPublicas(4, "Vino"),
+                new ProductoConPropiedadesPublicas(5, "Sal"),
+                new ProductoConPropiedadesPublicas(6, "Estufa"),
+            };
+
+            productosConE.InsertarProductosConE(productos);
+            productosConE.MostrarLista();
+            productosConE.LimpiarLista();
+            productosConE.MostrarLista();
         }
 
         public class Cliente
--- Program.cs
+++ Program.cs
@@ -4,22 +4,6 @@ namespace EjemploDeClase
     {
         static void Main(string[] args)
         {
-            ProductosConE productosConE = new ProductosConE();
-
-            ProductoConPropiedadesPublicas[] productos =
-            {
-                new ProductoConPropiedadesPublicas(1, "Yogurt descremado"),
-                new ProductoConPropiedadesPublicas(2, "Leche"),
-                new ProductoConPropiedadesPublicas(3, "Huevos"),
-                new ProductoConPropiedadesPublicas(4, "Vino"),
-                new ProductoConPropiedadesPublicas(5, "Sal"),
-                new ProductoConPropiedadesPublicas(6, "Estufa"),
-            };
-
-            productosConE.InsertarProductosConE(productos);
-            productosConE.MostrarLista();
-            productosConE.LimpiarLista();
-            productosConE.MostrarLista();
         }
     }
 }
